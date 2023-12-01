a5e66e9dbe6322cbb1a516045b523d52d41a047b
Felix <felix.huel6@gmail.com>
2023-12-01 15:56:53 +0100

    test

---
+++ 2023/day_one/exercise-1/answer.php
@@ -0,0 +1,28 @@
+<?php
+
+function calculateCalibrationValue($line) {
+    $firstDigit = intval($line[0]);
+    $lastDigit = intval(substr($line, -1));
+    $calibrationValue = $firstDigit * 10 + $lastDigit;
+
+    return $calibrationValue;
+}
+
+$csvFilePath = "input.csv";
+$rows = array_map('str_getcsv', file($csvFilePath));
+
+$totalCalibrationValue = 0;
+
+foreach ($rows as $row) {
+    if (count($row) > 0) {
+        $line = $row[0];
+        $calibrationValue = calculateCalibrationValue($line);
+        echo "Calibration value for '$line': $calibrationValue\n";
+
+        $totalCalibrationValue += $calibrationValue;
+    }
+}
+
+echo "Total Calibration Value: $totalCalibrationValue\n";
+
+?>
